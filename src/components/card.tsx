ef966c1b5041ed995e02dbd1d581e840e25a083f
Akihi-Sumi <akiakisander@gmail.com>
2022-05-31 12:58:55 +0900

    WaterShader

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,8 +4,7 @@ export const Card = () => {
     return (
         <div className="col">
             <div className="card shadow-sm">
-                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+                <img className="card-img-top" src="http://picsum.photos/200/200" width="100%" height="225" alt="Card" />
                 <div className="card-body">
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div className="d-flex justify-content-between align-items-center">
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import ReactDOM  from 'react-dom';
 import { createRoot } from "react-dom/client";
+import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './css/bootstrap.scss';
+import WaterShader from './WaterShader/ws-index';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 root.render(<App />);
-// render(<App />, rootElement);
+
+ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <div>
+          <Route path="/" element={<App/>} />
+          <Route path="/WaterShader/ws-index" element={<WaterShader />} />
+        </div>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+reportWebVitals();
